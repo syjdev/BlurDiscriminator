3bc3cf272d9558c207275dc211323a6566284e17
syjdev <syjdev@gmail.com>
2021-01-23 16:41:29 +0900

    add iOS demo project

---
+++ BlurDiscriminator-iOS/BlurDiscriminator-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BlurDiscriminator-iOS
+//
+//  Created by syjdev on 2021/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
