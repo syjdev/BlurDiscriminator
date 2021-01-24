d2ea9eebb84094e7ab1f2748bdd7526c5e18427b
syjdev <syjdev@gmail.com>
2021-01-24 20:51:10 +0900

    add a subviews, captureSession, blurDiscriminator in viewController

--- BlurDiscriminator-iOS/BlurDiscriminator-iOS/CaptureView/ViewController.swift
+++ BlurDiscriminator-iOS/BlurDiscriminator-iOS/CaptureView/ViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var previewContainerView: UIView!
+    @IBOutlet weak var pixelVarianceLabel: UILabel!
+    
+    private let captureSession = CaptureSession(devicePosition: .back)
+    private let blurDiscriminator: BlurDiscriminator
+    
+    
+    required init?(coder: NSCoder) {
+        do {
+            self.blurDiscriminator = try BlurDiscriminator()
+        } catch {
+            fatalError("failed to initialize BlurDiscriminator. - \(error)")
+        }
+        
+        super.init(coder: coder)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let videoPreviewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession.session)
+        videoPreviewLayer.frame.size = self.previewContainerView.frame.size
+        videoPreviewLayer.videoGravity = .resizeAspect
+        self.previewContainerView.layer.addSublayer(videoPreviewLayer)
     }
 
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.captureSession.delegate = self
+        self.captureSession.start()
+    }
 }
 
+
+extension ViewController: CaptureSessionDelegate {
+    func captureSession(_ captureSession: CaptureSession, didOutput sampleBuffer: CMSampleBuffer) {
+        
+    }
+}
