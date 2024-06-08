adaaa64741c0ed7eb5243d72f85fa5d185a51510
syjdev <syjdev@gmail.com>
2024-06-09 01:52:41 +0900

    - modify blurRatio interface - remove useless model files

--- Demo/iOS/BlurDiscriminatorKit/BlurDiscriminatorKit/BlurObservation.swift
+++ Demo/iOS/BlurDiscriminatorKit/BlurDiscriminatorKit/BlurObservation.swift
@@ -13,21 +13,22 @@ public final class BlurObservation {
     public let blurMap: CGImage
     public let grayscaledPixels: [UInt8]
     public let data: Data
-    public lazy var blurRatio: Float = {
-        let numberOfBlur: Float = grayscaledPixels.reduce(into: 0) { result, element in
-            if element > 127 {
-                result += 1
-            }
-        }
-        
-        return numberOfBlur / Float(grayscaledPixels.count)
-    }()
-    
+
     internal init(blurMap: CGImage,
                   grayscaledPixels: [UInt8],
                   data: Data = Data()) {
         self.blurMap = blurMap
         self.grayscaledPixels = grayscaledPixels
         self.data = data
+    }
+    
+    public func blurRatio(threshold: Int) -> Float {
+        let numberOfBlur: Float = grayscaledPixels.reduce(into: 0) { result, element in
+            if element > threshold {
+                result += 1
+            }
+        }
+        
+        return numberOfBlur / Float(grayscaledPixels.count)
     }
 }
